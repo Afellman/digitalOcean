dccf96e7948b045fd32f486ce45d46c6274d2f0e
afellman <andrew.fellman@protonmail.com>
2019-02-07 20:33:00 -0500

    first push

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 80;
+const path = require('path');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get('/', (req, res)=>{
+  res.sendFile(path.join(__dirname, "index.html"));
+})
+
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
